2c6a0ad73e7b50d0439e44abcbd31a02fc8ffc25
Ruben <rdom76@gmail.com>
2015-04-12 22:27:54 -0600

    Initial support for in flight cards as tabbed fragments

---
+++ app/src/main/java/edu/utep/cs/pickax/fpms/TimeCard.java
@@ -0,0 +1,30 @@
+package edu.utep.cs.pickax.fpms;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeCard extends Fragment {
+
+
+    public TimeCard() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time_card, container, false);
+    }
+
+
+}
