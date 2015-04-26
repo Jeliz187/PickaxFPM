d94baaeee9942498e06bcbd044ed0fa96a1b72f4
Ruben <ruben>
2015-04-26 11:57:05 -0600

    Removed map things

--- app/src/main/java/edu/utep/cs/pickax/fpms/InFlight.java
+++ app/src/main/java/edu/utep/cs/pickax/fpms/InFlight.java
@@ -4,76 +4,26 @@ InFlght handles realtime data display and the Google Glass
 package edu.utep.cs.pickax.fpms;
 
 import android.content.Context;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextClock;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import static android.support.v4.app.Fragment.instantiate;
 
 //TODO Fix crash when returning to map activity
 //TODO: Stop timers when leaving activity
-public class InFlight extends ActionBarActivity implements GoogleMap.OnFragmentInteractionListener {
+public class InFlight extends ActionBarActivity{
     private Context context;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_flight);
-        this.context = this;
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                getSupportActionBar().setTitle(mSectionsPagerAdapter.getPageTitle(position));
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        getSupportActionBar().setTitle(mSectionsPagerAdapter.getPageTitle(0));
 
         //TODO use Display class here (?) to update labels
     }
@@ -99,60 +49,6 @@ public class InFlight extends ActionBarActivity implements GoogleMap.OnFragmentI
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            Fragment fragment = null;
-            switch (position) {
-                case 0:
-                    fragment = instantiate(context, MapCard.class.getName());
-                    break;
-                case 1:
-                    fragment = instantiate(context, WeatherCard.class.getName());
-                    break;
-                case 2:
-                    fragment = instantiate(context, TimeCard.class.getName());
-                    break;          }
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            String name = getResources().getString(R.string.title_activity_in_flight);
-            switch (position) {
-                case 0:
-                    return name + ": Map";
-                case 1:
-                    return name + ": Weather Map";
-                case 2:
-                    return name + ": Time";
-            }
-            return null;
-        }
-
     }
 
 
